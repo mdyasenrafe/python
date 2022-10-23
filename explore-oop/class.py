ff3a1e74cc468ca2928fe24aaac81b21b19d9157
Md yasen rafe <mdyasenrafe@gmail.com>
2022-10-23 22:58:32 +0600

    project start

--- explore-oop/class.py
+++ explore-oop/class.py
@@ -1,14 +1,5 @@
-class Phone:
-    def __init__(self, brand, model, price):
-        self.brand = brand
-        self.model = model
-        self.price = price
-
-    def increase_price(self, amount):
-        self.last_price = self.price
-        self.price = self.price + amount
+class Fruit:
+    pass
 
 
-my_phone = Phone("Apple", "iPhone 12", 80000)
-my_phone.increase_price(1000)
-print(my_phone.__dict__)
+print(Fruit.__name__)
